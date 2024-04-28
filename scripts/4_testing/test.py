089e72a3ff8baf71614403d49287a3886ba4daf7
EtienneChollet <etiennechollet2626@gmail.com>
2024-04-28 09:29:54 -0400

    [FEAT] multiple input volumes

--- scripts/4_testing/test.py
+++ scripts/4_testing/test.py
@@ -1,3 +1,4 @@
+import gc
 import time
 import veritas
 import torch
@@ -7,39 +8,47 @@ def main(args):
     versions = args.version
     versions = versions.split(',')
     input = args.input
+    inputs = input.split(',')
     patch_size = args.patch_size
     redundancy = args.redundancy
     checkpoint = args.checkpoint
 
     for v in versions:
-        t1 = time.time()
-        with torch.no_grad():
-            unet = veritas.Unet(
-                model_dir='models',
-                version_n=v,
-                device='cuda'
-                )
-            
-            unet.load(type=checkpoint, mode='test')
+        for i in inputs:
+            t1 = time.time()
+            with torch.no_grad():
+                unet = veritas.Unet(
+                    model_dir='models',
+                    version_n=v,
+                    device='cuda'
+                    )
+                
+                unet.load(type=checkpoint, mode='test')
 
-            prediction = veritas.data.RealOctPredict(
-                input=input,
-                trainee=unet.trainee,
-                dtype=torch.float32,
-                device='cuda',
-                patch_size=patch_size,
-                redundancy=redundancy,
-                normalize=False,
-                pad_it=True,
-                padding_method='reflect',
-                normalize_patches=True,
+                prediction = veritas.data.RealOctPredict(
+                    input=i,
+                    trainee=unet.trainee,
+                    dtype=torch.float32,
+                    device='cuda',
+                    patch_size=patch_size,
+                    redundancy=redundancy,
+                    normalize=False,
+                    pad_it=True,
+                    padding_method='reflect',
+                    normalize_patches=True,
 
-                )
-            prediction.predict_on_all()
-            out_path = f"{unet.version_path}/predictions"
-            prediction.save_prediction(dir=out_path)
-            t2 = time.time()
-            print(f"Process took {round((t2-t1)/60, 2)} min")
+                    )
+                prediction.predict_on_all()
+                out_path = f"{unet.version_path}/predictions"
+                prediction.save_prediction(dir=out_path)
+                t2 = time.time()
+                print(f"Process took {round((t2-t1)/60, 2)} min")
+                print('#' * 30, '\n')
+                del unet
+                del prediction
+                torch.cuda.empty_cache()
+                gc.collect()
+
 
 
 if __name__ == '__main__':
@@ -47,13 +56,13 @@ if __name__ == '__main__':
 
     parser.add_argument('-v', '--version', type=str, default='1',
                         help='Model version to test (default: 1). Can test many versions seperated by commas.')
+    parser.add_argument('-i', '--input', type=str, required=False,
+                        default='/autofs/cluster/octdata2/users/epc28/data/caroline_data/I46_Somatosensory_20um_crop.nii',
+                        help='Path to NIfTI data. Can test many input files seperated by commas.')
     parser.add_argument('-p', '--patch_size', type=int, default=128,
                         help='Size of the Unet input layer and desired patch size (default: 128).')
     parser.add_argument('-r', '--redundancy', type=int, default=3,
                         help='Redundancy factor for prediction overlap (default: 3).')
-    parser.add_argument('-i', '--input', type=str, required=False,
-                        default='/autofs/cluster/octdata2/users/epc28/data/caroline_data/I46_Somatosensory_20um_crop.nii',
-                        help='Path to NIfTI data.')
     parser.add_argument('-c', '--checkpoint', type=str, default='best',
                         help='Checkpoint to load. "best" or "last".')
     
--- scripts/4_testing/test.sh
+++ scripts/4_testing/test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 #jobsubmit -A psoct -p dgx-a100 -m 20G -t 7-00:00:00 -c 4 -G 1 -o logs/test.log python3 /autofs/cluster/octdata2/users/epc28/veritas/sandbox/dataloading_debugging.py;
-jobsubmit -A psoct -p dgx-a100 -m 10G -t 7-00:00:00 -c 6 -G 1 -o logs/test/test_version-batch.log python3 /autofs/cluster/octdata2/users/epc28/veritas/scripts/4_testing/test.py --version 1,11,111,2,22,222,3,33,333,4,44,444,5,55,555,6,66,666,7,77,777,8,88,888,9,99,999,101,102,103;
+jobsubmit -A psoct -p dgx-a100 -m 75G -t 7-00:00:00 -c 6 -G 1 -o logs/test/caa26-frontal.log python3 /autofs/cluster/octdata2/users/epc28/veritas/scripts/4_testing/test.py -v 2,22,222,3,33,333,4,44,444,5,55,555,6,66,666,7,77,777,8,88,888,101,102,103 -i /autofs/cluster/octdata2/users/epc28/data/CAA/caa26/frontal/caa26_frontal.nii;
 watch -n 0.1 "squeue -u $USER"
